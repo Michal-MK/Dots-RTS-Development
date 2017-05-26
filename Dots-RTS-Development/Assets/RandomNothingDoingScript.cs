cefc1f22b663ca0259443cf7cc7054740e8b2d54
Slinta <Slinta000@gmail.com>
2017-05-26 21:51:53 +0200

    Added a script to the camera
    Script does nothing

---
+++ Dots-RTS-Development/Assets/RandomNothingDoingScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomNothingDoingScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//This is where the fun begins
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
