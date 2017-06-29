7c7cd16a69e5c381ba6fc07e89b799c5bf215df4
Slinta <Slinta000@gmail.com>
2017-06-29 23:33:18 +0200

    HOTFIX
    Switched android and PC in LevelSelectScript

--- Dots-RTS-Development/Assets/Scripts/Menu & Wrappers/LevelSelectScript.cs
+++ Dots-RTS-Development/Assets/Scripts/Menu & Wrappers/LevelSelectScript.cs
@@ -9,15 +9,13 @@ public class LevelSelectScript : MonoBehaviour {
 	public InputField manualFileNameIF;
 	// Use this for initialization
 #if UNITY_ANDROID
-	public void  loadButtonPress () {
-		PlayerPrefs.SetString("LoadLevelFilePath", Application.streamingAssetsPath + "/Saves/" + fileName + ".phage");
-		SceneManager.LoadScene("LevelPlayer");
-	}
-
-#else
-
 	public void loadButtonPress() {
 		PlayerPrefs.SetString("LoadLevelFilePath", Application.persistentDataPath + "/Saves/" + manualFileNameIF.text + ".phage");
+		SceneManager.LoadScene("LevelPlayer");
+	}
+#else
+	public void loadButtonPress() {
+		PlayerPrefs.SetString("LoadLevelFilePath", Application.streamingAssetsPath + "/Saves/" + manualFileNameIF.text + ".phage");
 		SceneManager.LoadScene("LevelPlayer");
 	}
 #endif
