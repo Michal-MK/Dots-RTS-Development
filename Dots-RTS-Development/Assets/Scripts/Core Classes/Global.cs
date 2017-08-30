4581a5faeb2df34ebfe4a00b199a810bed6192de
Michal <michalhz159@gmail.com>
2017-08-30 12:34:41 +0200

    Updated versioning.

--- Dots-RTS-Development/Assets/Scripts/Core Classes/Global.cs
+++ Dots-RTS-Development/Assets/Scripts/Core Classes/Global.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Global {
 	public static bool baseLoaded = false;
