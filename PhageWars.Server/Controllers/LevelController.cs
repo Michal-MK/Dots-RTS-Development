389d840f90b250aa8cbf5cf5927b5c83fe34cf00
Michal-MK <michalhz159@gmail.com>
2021-06-13 12:38:04 +0200

    Added PhageWars LevelServer

---
+++ PhageWars.Server/Controllers/LevelController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Net.Mime;
+
+namespace PhageWars.Server.Controllers {
+	[ApiController]
+	[Route("[controller]")]
+	public class LevelController
+		: ControllerBase {
+
+		private readonly ILogger<LevelController> _logger;
+
+		public LevelController(ILogger<LevelController> logger) {
+			_logger = logger;
+		}
+
+		[HttpGet]
+		public FileContentResult Level(string name) {
+			return new FileContentResult(FileManager.GetLevel(name), MediaTypeNames.Application.Octet) { FileDownloadName = name };
+		}
+	}
+}
---
+++ PhageWars.Server/FileManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhageWars.Server {
+	public class FileManager {
+
+		const string LEVELS_PATH = "levels";
+
+		private static string BaseLevelsPath => Path.Combine(AppDomain.CurrentDomain.BaseDirectory, LEVELS_PATH);
+
+		static FileManager() {
+			if (!Directory.Exists(BaseLevelsPath)) {
+				Directory.CreateDirectory(BaseLevelsPath);
+			}
+		}
+
+		public static byte[] GetLevel(string levelName) {
+			string path = Path.Combine(BaseLevelsPath, levelName);
+			if (File.Exists(path)) {
+				return File.ReadAllBytes(path);
+			}
+			return default;
+		}
+
+		public static string GetLevelAbsPath(string levelName) {
+			string path = Path.Combine(BaseLevelsPath, levelName);
+			if (File.Exists(path)) {
+				return path;
+			}
+			return null;
+		}
+	}
+}
